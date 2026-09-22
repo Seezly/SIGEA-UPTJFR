d00a8ad705f471324ad58ba499a035b4f628e1c9
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-09-22 19:26:23 -0400

    fix: add dropIfExists to additional tables inside migrations and fixed typo in hiring migration dropIfExists method

--- database/migrations/2026_09_22_230019_create_employees_table.php
+++ database/migrations/2026_09_22_230019_create_employees_table.php
@@ -55,5 +55,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('employees');
+        Schema::dropIfExists('employee_documents');
+        Schema::dropIfExists('employee_records');
     }
 };
