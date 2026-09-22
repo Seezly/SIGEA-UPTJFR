3ffb3defffaa9996e4a6efe57556e669a9fd618b
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-09-22 19:20:54 -0400

    fix: add dropIfExists to additional tables inside migrations

--- database/migrations/2026_09_22_164919_create_applicant_profiles_table.php
+++ database/migrations/2026_09_22_164919_create_applicant_profiles_table.php
@@ -39,6 +39,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('documents');
         Schema::dropIfExists('applicant_profiles');
     }
 };
