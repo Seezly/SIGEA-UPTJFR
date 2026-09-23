2b467572ec02ff362ad51cfd87fcdfeef237c2f1
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-09-22 20:19:28 -0400

    feat: add configuration and backup database migrations and update DER schema

---
+++ database/migrations/2026_09_23_000418_create_backup_runs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('backup_runs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('requested_by')->constrained('users', 'id');
+            $table->string('type', 100);
+            $table->string('status', 10);
+            $table->string('file_path');
+            $table->integer('size_bytes');
+            $table->string('checksum');
+            $table->timestamp('started_at');
+            $table->timestamp('completed_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('backup_runs');
+    }
+};
