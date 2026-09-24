cb8121af36981990dbabcbd3b540180bd4079005
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-09-24 17:24:28 -0400

    fix: module column unique constraint typo

--- database/migrations/2026_09_23_000342_create_module_settings_table.php
+++ database/migrations/2026_09_23_000342_create_module_settings_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->boolean('is_active')->default(true);
             $table->timestamps();
 
-            $table->unique(['module', 'key']);
+            $table->unique(['module_id', 'key']);
         });
     }
 
